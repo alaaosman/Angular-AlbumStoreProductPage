465b84e2af3436b80ecf002631c3157fb068eff2
Alaa Osman <alaa.osmana@gmail.com>
2019-10-27 14:57:22 +0200

    Product Info

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from './../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+  albumInfo;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => {
+      // console.log(response);
+      this.albumInfo = response
+    });
   }
 
 }
